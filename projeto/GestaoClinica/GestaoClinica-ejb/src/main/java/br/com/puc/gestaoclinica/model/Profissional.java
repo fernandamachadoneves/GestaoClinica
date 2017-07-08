a2fc07728777ddb675dab5c708c58f73684f7463
Admin <Admin@4504>
2017-07-07 23:47:29 -0300

    entity

--- projeto/GestaoClinica/GestaoClinica-ejb/src/main/java/br/com/puc/gestaoclinica/model/Profissional.java
+++ projeto/GestaoClinica/GestaoClinica-ejb/src/main/java/br/com/puc/gestaoclinica/model/Profissional.java
@@ -1,5 +1,91 @@
 package br.com.puc.gestaoclinica.model;
 
-public class Profissional {
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotEmpty;
+
+@Entity
+@XmlRootElement
+@Table(name = "Profissional", uniqueConstraints = @UniqueConstraint(columnNames = "email"))
+public class Profissional implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotNull
+	@Size(min = 1, max = 25)
+	@Pattern(regexp = "[^0-9]*", message = "So pode conter numeros")
+	private String nome;
+
+	@NotNull
+	@NotEmpty
+	@Email
+	private String email;
+	
+	@NotNull
+	@NotEmpty
+	private String crm;
+
+	@NotNull
+	@NotEmpty
+	private String telefone;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getCrm() {
+		return crm;
+	}
+
+	public void setCrm(String crm) {
+		this.crm = crm;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	
+	
+
 
 }
