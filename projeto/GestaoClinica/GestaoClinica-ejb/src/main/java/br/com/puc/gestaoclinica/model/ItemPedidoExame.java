2b7fb5574b4fd89ecfe9f358c022a21e898bd66a
admin <admin@WIN-7PRO>
2017-07-26 20:27:57 -0300

    exames

---
+++ projeto/GestaoClinica/GestaoClinica-ejb/src/main/java/br/com/puc/gestaoclinica/model/ItemPedidoExame.java
@@ -0,0 +1,70 @@
+package br.com.puc.gestaoclinica.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@Entity
+@XmlRootElement
+@Table(name = "ItemPedidoExame")
+public class ItemPedidoExame implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@ManyToOne
+	@JoinColumn(name = "pedidoExame")
+	private PedidoExame pedidoExame;
+	
+	@ManyToOne
+	@JoinColumn(name = "exame")
+	private Exame exame;
+	
+	@Column(name = "resultado")
+	private String resultado;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public PedidoExame getPedidoExame() {
+		return pedidoExame;
+	}
+
+	public void setPedidoExame(PedidoExame pedidoExame) {
+		this.pedidoExame = pedidoExame;
+	}
+
+	public Exame getExame() {
+		return exame;
+	}
+
+	public void setExame(Exame exame) {
+		this.exame = exame;
+	}
+
+	public String getResultado() {
+		return resultado;
+	}
+
+	public void setResultado(String resultado) {
+		this.resultado = resultado;
+	}
+	
+	
+}
---
+++ projeto/GestaoClinica/GestaoClinica-ejb/src/main/java/br/com/puc/gestaoclinica/model/PedidoExame.java
@@ -0,0 +1,80 @@
+package br.com.puc.gestaoclinica.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@Entity
+@XmlRootElement
+@Table(name = "PedidoExame")
+public class PedidoExame implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "dataPedido")
+	private Date dataPedido;
+	
+	@ManyToOne
+	@JoinColumn(name = "paciente")
+	private Paciente paciente;
+	
+	@ManyToOne
+	@JoinColumn(name = "profissional")
+	private Profissional profissional;
+	
+	@Column(name = "ativo")
+	private Boolean ativo;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Paciente getPaciente() {
+		return paciente;
+	}
+
+	public void setPaciente(Paciente paciente) {
+		this.paciente = paciente;
+	}
+
+	public Profissional getProfissional() {
+		return profissional;
+	}
+
+	public void setProfissional(Profissional profissional) {
+		this.profissional = profissional;
+	}
+
+	public Boolean getAtivo() {
+		return ativo;
+	}
+
+	public void setAtivo(Boolean ativo) {
+		this.ativo = ativo;
+	}
+
+	public Date getDataPedido() {
+		return dataPedido;
+	}
+
+	public void setDataPedido(Date dataPedido) {
+		this.dataPedido = dataPedido;
+	}
+}
